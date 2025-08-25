35abe7e9944a4f09e3029b81d03a81ab8b88ebb2
DanCS <mil.cua@teczion.edu.gt>
2025-08-25 15:00:33 -0600

    Palabra y descripción

--- Pruebapalabra.py
+++ Pruebapalabra.py
@@ -11,3 +11,15 @@ cr =baseDeDatos.cursor()
 app = Tk()
 app.title("Juego Adivina Palabra")
 
+neword =Label(app, text="Ingrese una palabra")
+descripcion =Label(app, text="Ingrese la descripción")
+
+palabra = Entry(app)
+Descrip = Text(app, width=15, height=3)
+
+neword.grid(row=0, column=0, pady=5, padx=5)
+descripcion.grid(row=1, column=0, pady=5, padx=5)
+palabra.grid(row=0, column=1, pady=5, padx=5)
+Descrip.grid(row=1, column=1, pady=5, padx=5)
+
+app.mainloop()
