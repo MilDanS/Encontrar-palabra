100100f3be276c9069288372ec52e2d255eaf550
DanCS <mil.cua@teczion.edu.gt>
2025-08-25 14:42:48 -0600

    Datos e interfaz

--- Pruebapalabra.py
+++ Pruebapalabra.py
@@ -1,5 +1,13 @@
 from sqlite3 import *
 from tkinter import messagebox
 from tkinter import *
+from tkinter import ttk
 
-def agregarpalabras ():
+#def agregarpalabras ():
+
+baseDeDatos = connect("EncontrarPalabra.db")
+cr =baseDeDatos.cursor()
+
+app = Tk()
+app.title("Juego Adivina Palabra")
+
