29023c2ba7340b8f44fb5cbd1b8cce7feb0e82e5
DanCS <mil.cua@teczion.edu.gt>
2025-08-25 16:14:38 -0600

    Ingresar palabras

--- Pruebapalabra.py
+++ Pruebapalabra.py
@@ -1,9 +1,19 @@
 from sqlite3 import *
-from tkinter import messagebox
+#from tkinter import messagebox
 from tkinter import *
-from tkinter import ttk
+#from tkinter import ttk
 
-#def agregarpalabras ():
+def agregarpalabra():
+    nuevap = palabra.get()
+    des = str(Descrip.get("1.0", "end"))
+    verpalabra.configure(text=f"La palabra es: {nuevap}")
+    verdes.configure(text=f"La descripción es: {des}")
+
+
+    cr.execute('''
+            INSERT INTO palabras(nombre, descripcion)
+            VALUES(?,?)''', (nuevap, des))
+    baseDeDatos.commit()
 
 baseDeDatos = connect("EncontrarPalabra.db")
 cr =baseDeDatos.cursor()
@@ -17,9 +27,17 @@ descripcion =Label(app, text="Ingrese la descripción")
 palabra = Entry(app)
 Descrip = Text(app, width=15, height=3)
 
+verpalabra= Label(app, text="La palabra es: ")
+verdes = Label(app, text="La descrpción es: ")
+
+resul = Button(app, text="Agregar", command=agregarpalabra)
+
 neword.grid(row=0, column=0, pady=5, padx=5)
 descripcion.grid(row=1, column=0, pady=5, padx=5)
 palabra.grid(row=0, column=1, pady=5, padx=5)
 Descrip.grid(row=1, column=1, pady=5, padx=5)
+resul.grid(row=2, column=0, pady=5, padx=5)
+verpalabra.grid(row=3, column=0, padx=5, pady=5)
+verdes.grid(row=4, column=0, padx=5, pady=5)
 
 app.mainloop()
