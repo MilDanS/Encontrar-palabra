114f774a3a93d94f49158d4d6be7a6c5e31538a1
DanCS <mil.cua@teczion.edu.gt>
2025-09-01 15:55:20 -0600

    Verificar y jugar

--- Jugar.py
+++ Jugar.py
@@ -14,7 +14,7 @@ Fallos = 0
 Word = " "
 Desc = " "
 
-def jugar ():
+def verificar ():
     global Intentos, Fallos
 
     entrada = juego.get().strip()
@@ -35,9 +35,31 @@ def jugar ():
             messagebox.showerror("¡Perdiste!", f"Se acabaron los intentos. La palabra era: {Word}")
             juego.delete(0, END)
 
+def jugar ():
+    global Intentos, Fallos, Word, Desc
+    Intentos = 3
+    Fallos = 0
+
+    numero = randint(1, 7)
+    cr.execute('''SELECT nombre, descripcion FROM palabras 
+                WHERE id = ?''', (numero, ))
+
+    resultado = cr.fetchone()
+    Word  = resultado[0]
+    Desc = resultado[1]
+
+    juego.delete(0, END)
+    descrip.configure(text=Desc)
+
 juego = Entry(app)
 descrip = Label(app, text=" ")
 jugarb = Button(app, text="Jugar", command=jugar)
+int = Button(app, text="Verificar", command=verificar)
+
+juego.grid(row= 5, column=5, pady=5, padx=5)
+descrip.grid(row= 6, column=5, pady=5, padx=5)
+jugarb.grid(row= 7, column=5, pady=5, padx=5)
+int.grid(row=8, column=5,padx=5, pady=5)
 
 
 app.mainloop()
