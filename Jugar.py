b2e6e06ee8d6ef709848af780e8d05b5d2cde0ba
DanCS <mil.cua@teczion.edu.gt>
2025-09-02 03:28:15 -0600

    Lo ultimo de jugar

--- Jugar.py
+++ Jugar.py
@@ -3,75 +3,76 @@ from sqlite3 import *
 from tkinter import *
 from tkinter import messagebox
 
-baseDeDatos = connect("EncontrarPalabra.db")
-cr = baseDeDatos.cursor()
+def lomalo ():
+    baseDeDatos = connect("EncontrarPalabra.db")
+    cr = baseDeDatos.cursor()
 
-app = Tk()
-app.title("Juego Adivina Palabra")
+    app = Tk()
+    app.title("Juego Adivina Palabra")
 
-Intentos = 3
-Fallos = 0
-Word = " "
-Desc = " "
-
-def salir():
-    app.destroy()
-
-def verificar ():
-    global Intentos, Fallos
-
-    entrada = juego.get().strip()
-
-    if entrada == "":
-        messagebox.showwarning("Entrada vacía", "Por favor, escribe una palabra.")
-        return
-    elif entrada.lower() == Word.lower():
-        messagebox.showinfo("¡Ganaste!", f"Felicidades, la palabra era: {Word}")
-        juego.delete(0, END)
-    else:
-        Intentos -= 1
-        Fallos += 1
-
-        if Intentos > 0:
-            messagebox.showinfo("Incorrecto", f"Palabra incorrecta. Te quedan {Intentos} intentos.")
-            juego.delete(0, END)
-        else:
-            messagebox.showerror("¡Perdiste!", f"Se acabaron los intentos. La palabra era: {Word}")
-            juego.delete(0, END)
-
-def jugar ():
-    global Intentos, Fallos, Word, Desc
     Intentos = 3
     Fallos = 0
+    Word = " "
+    Desc = " "
 
-    numero = randint(1, 7)
-    cr.execute('''SELECT nombre, descripcion FROM palabras 
-                WHERE id = ?''', (numero, ))
+    def salir():
+        app.destroy()
 
-    resultado = cr.fetchone()
-    Word  = resultado[0]
-    Desc = resultado[1]
+    def verificar ():
+        global Intentos, Fallos
 
-    juego.delete(0, END)
-    descrip.configure(text=Desc)
+        entrada = juego.get().strip()
 
-juego = Entry(app)
-descrip = Label(app, text=" ")
-jugarb = Button(app, text="Jugar", command=jugar)
-int = Button(app, text="Verificar", command=verificar)
-next = Button(app, text="Salir", command=salir)
+        if entrada == "":
+            messagebox.showwarning("Entrada vacía", "Por favor, escribe una palabra.")
+            return
+        elif entrada.lower() == Word.lower():
+            messagebox.showinfo("¡Ganaste!", f"Felicidades, la palabra era: {Word}")
+            juego.delete(0, END)
+        else:
+            Intentos -= 1
+            Fallos += 1
 
-Label(app, text="Ingresa la palabra:").grid(row=1, column=0, padx=5, pady=5)
+            if Intentos > 0:
+                messagebox.showinfo("Incorrecto", f"Palabra incorrecta. Te quedan {Intentos} intentos.")
+                juego.delete(0, END)
+            else:
+                messagebox.showerror("¡Perdiste!", f"Se acabaron los intentos. La palabra era: {Word}")
+                juego.delete(0, END)
 
-Label(app, text="Descripción:").grid(row=0, column=0, padx=5, pady=5)
+    def jugar ():
+        global Intentos, Fallos, Word, Desc
+        Intentos = 3
+        Fallos = 0
 
-juego.grid(row=1, column=0, columnspan=3, pady=5, padx=5)
-descrip.grid(row=0, column=1, pady=5, padx=5)
-jugarb.grid(row=2, column=0, pady=5, padx=5)
-int.grid(row=2, column=1,padx=5, pady=5)
-next.grid(row=2, column=2, pady=5, padx=5)
+        numero = randint(1, 7)
+        cr.execute('''SELECT nombre, descripcion FROM palabras 
+                    WHERE id = ?''', (numero, ))
 
-app.mainloop()
+        resultado = cr.fetchone()
+        Word  = resultado[0]
+        Desc = resultado[1]
+
+        juego.delete(0, END)
+        descrip.configure(text=Desc)
+
+    juego = Entry(app)
+    descrip = Label(app, text=" ")
+    jugarb = Button(app, text="Jugar", command=jugar)
+    int = Button(app, text="Verificar", command=verificar)
+    next = Button(app, text="Salir", command=salir)
+
+    Label(app, text="Ingresa la palabra:").grid(row=1, column=0, padx=5, pady=5)
+
+    Label(app, text="Descripción:").grid(row=0, column=0, padx=5, pady=5)
+
+    juego.grid(row=1, column=0, columnspan=3, pady=5, padx=5)
+    descrip.grid(row=0, column=1, pady=5, padx=5)
+    jugarb.grid(row=2, column=0, pady=5, padx=5)
+    int.grid(row=2, column=1,padx=5, pady=5)
+    next.grid(row=2, column=2, pady=5, padx=5)
+
+    app.mainloop()
 
 
 
--- ingresarpalabra.py
+++ ingresarpalabra.py
@@ -2,39 +2,39 @@ from sqlite3 import *
 from customtkinter import *
 from customtkinter import CTkTextbox
 
+def lobueno():
+    def agregarpalabra():
+        nuevap = palabra.get()
+        des = str(Descrip.get("1.0", "end"))
 
-def agregarpalabra():
-    nuevap = palabra.get()
-    des = str(Descrip.get("1.0", "end"))
+        cr.execute('''
+                INSERT INTO palabras(nombre, descripcion)
+                VALUES(?,?)''', (nuevap, des))
+        baseDeDatos.commit()
 
-    cr.execute('''
-            INSERT INTO palabras(nombre, descripcion)
-            VALUES(?,?)''', (nuevap, des))
-    baseDeDatos.commit()
+    def salir():
+        app.destroy()
 
-def salir():
-    app.destroy()
+    baseDeDatos = connect("EncontrarPalabra.db")
+    cr =baseDeDatos.cursor()
 
-baseDeDatos = connect("EncontrarPalabra.db")
-cr =baseDeDatos.cursor()
+    app = CTk()
+    app.title("Juego Adivina Palabra")
 
-app = CTk()
-app.title("Juego Adivina Palabra")
+    neword =CTkLabel(app, text="Ingrese una palabra")
+    descripcion =CTkLabel(app, text="Ingrese la descripción")
 
-neword =CTkLabel(app, text="Ingrese una palabra")
-descripcion =CTkLabel(app, text="Ingrese la descripción")
+    palabra = CTkEntry(app, fg_color="white", text_color="black", width=150)
+    Descrip = CTkTextbox(app, width=150, height=50, fg_color="white", text_color="black")
 
-palabra = CTkEntry(app, fg_color="white", text_color="black", width=150)
-Descrip = CTkTextbox(app, width=150, height=50, fg_color="white", text_color="black")
+    resul = CTkButton(app, text="Agregar", command=agregarpalabra)
+    Salir=CTkButton(app, text="Salir", command=salir)
 
-resul = CTkButton(app, text="Agregar", command=agregarpalabra)
-Salir=CTkButton(app, text="Salir", command=salir)
+    neword.grid(row=0, column=0, pady=5, padx=5)
+    descripcion.grid(row=1, column=0, pady=5, padx=5)
+    palabra.grid(row=0, column=1, pady=5, padx=5)
+    Descrip.grid(row=1, column=1, pady=5, padx=5)
+    resul.grid(row=2, column=0, pady=5, padx=5)
+    Salir.grid(row=2, column=1, padx=5, pady=5)
 
-neword.grid(row=0, column=0, pady=5, padx=5)
-descripcion.grid(row=1, column=0, pady=5, padx=5)
-palabra.grid(row=0, column=1, pady=5, padx=5)
-Descrip.grid(row=1, column=1, pady=5, padx=5)
-resul.grid(row=2, column=0, pady=5, padx=5)
-Salir.grid(row=2, column=1, padx=5, pady=5)
-
-app.mainloop()
+    app.mainloop()
