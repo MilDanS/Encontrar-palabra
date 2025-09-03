59cc299d27073e7f846011df62f9a5cbbe93457e
DanCS <mil.cua@teczion.edu.gt>
2025-09-03 02:24:02 -0600

    SE comento el otro programa

--- Jugar.py
+++ Jugar.py
@@ -4,36 +4,36 @@ from tkinter import *
 from tkinter import messagebox
 
 def lomalo ():
-    baseDeDatos = connect("EncontrarPalabra.db")
+    baseDeDatos = connect("EncontrarPalabra.db")#SE conecta con la base de datos
     cr = baseDeDatos.cursor()
 
-    app = Tk()
+    app = Tk()#SE creo la pantalla principal
     app.title("Juego Adivina Palabra")
 
-    Intentos = 3
+    Intentos = 3#variables principales
     Fallos = 0
     Word = " "
     Desc = " "
 
     def salir():
-        app.destroy()
+        app.destroy()#Funcion para salir
 
     def verificar ():
-        global Intentos, Fallos, Word
+        global Intentos, Fallos, Word #Usa las variables globales para verificar la respuesta.
 
-        entrada = juego.get().strip()
+        entrada = juego.get().strip()#Obtiene el texto ingresado por el jugador y elimina espacios.
 
         if entrada == "":
-            messagebox.showwarning("Entrada vacía", "Por favor, escribe una palabra.")
+            messagebox.showwarning("Entrada vacía", "Por favor, escribe una palabra.")#Si el campo está vacío, muestra una advertencia.
             return
         elif entrada.lower() == Word.lower():
-            messagebox.showinfo("¡Ganaste!", f"Felicidades, la palabra era: {Word}")
-            juego.delete(0, END)
+            messagebox.showinfo("¡Ganaste!", f"Felicidades, la palabra era: {Word}")#Si se gana, mumetra un mensaje
+            juego.delete(0, END)#Limpia el espacio
         else:
-            Intentos -= 1
+            Intentos -= 1#Si es incorrecta, reduce los intentos y aumenta los fallos.
             Fallos += 1
 
-            if Intentos > 0:
+            if Intentos > 0:#Muestra mensaje de error o derrota según los intentos restantes.
                 messagebox.showinfo("Incorrecto", f"Palabra incorrecta. Te quedan {Intentos} intentos.")
                 juego.delete(0, END)
             else:
@@ -42,17 +42,17 @@ def lomalo ():
 
     def jugar ():
         global Intentos, Fallos, Word, Desc
-        Intentos = 3
+        Intentos = 3#Reinicia los contadores.
         Fallos = 0
 
-        numero = randint(1, 7)
+        numero = randint(1, 7)#Selecciona aleatoriamente una palabra por su id en la base de datos.
         cr.execute('''SELECT nombre, descripcion FROM palabras 
                     WHERE id = ?''', (numero, ))
 
         resultado = cr.fetchone()
         Word  = resultado[0]
         Desc = resultado[1]
-
+        #Limpia el campo de entrada y muestra la nueva descripción.
         juego.delete(0, END)
         descrip.configure(text=Desc)
 
